d234dfc6c015802afc988bc7ae1594a5f6936c4a
ProStrike2448 <ostapenko.andrey03@gmail.com>
2023-06-03 08:20:22 +0300

    integrating cms

---
+++ sanity/schemas/course.ts
@@ -0,0 +1,36 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "course",
+  title: "Course",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {
+          title: 'Text',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    }),
+  ],
+});
---
+++ sanity/schemas/lesson.ts
@@ -0,0 +1,36 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "lesson",
+  title: "Lesson",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'text',
+      title: 'Text',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    }),
+  ],
+});
