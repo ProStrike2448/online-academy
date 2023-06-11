0a6c3fcd805a38518b44a70371801eed5c748ac3
ProStrike2448 <ostapenko.andrey03@gmail.com>
2023-06-11 18:39:10 +0300

    test4 deploy build

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -15,9 +15,7 @@ export default async function SignIn() {
 								<button
 									className='w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
 									onClick={() =>
-										void signIn(provider.id, {
-											callbackUrl: 'http://localhost:3000/'
-										})
+										void signIn(provider.id)
 									}
 								>
 									Sign in with {provider.name}
